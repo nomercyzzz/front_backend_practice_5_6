01746570f563af9c654859c3060dc4336fab8093
nomercyzzz <ghostnomercy007@gmail.com>
2025-02-26 14:28:46 +0300

    Добавлен каталог товаров с использованием JSON

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Отдача JSON-файла с данными о товарах
+app.get('/xxx.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'xxx.json'));
+});
+
 // Запуск сервера
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
